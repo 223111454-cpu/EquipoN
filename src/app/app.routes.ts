59fee2f51ff21323d38c9c1f4534d88156f64b3e
Codeflex-Cloud <info@codeflex.com.co>
2025-09-11 08:35:27 -0600

    Actualiza Componentes , Login , Registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { Registro } from './components/registro/registro';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'Registro', component: Registro },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
